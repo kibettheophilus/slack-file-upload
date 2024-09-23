7bbbaacd845b6d05b3d7dbd6866a178274d6044b
theophiluskibet <kibettheophilus@gmail.com>
2024-09-23 14:35:52 +0300

    bundle

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,17 +1,17 @@
-/**
- * Unit tests for the action's entrypoint, src/index.ts
- */
+// /**
+//  * Unit tests for the action's entrypoint, src/index.ts
+//  */
 
-import * as main from '../src/main'
+// import * as main from '../src/main'
 
-// Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
+// // Mock the action's entrypoint
+// const runMock = jest.spyOn(main, 'run').mockImplementation()
 
-describe('index', () => {
-  it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
+// describe('index', () => {
+//   it('calls run when imported', async () => {
+//     // eslint-disable-next-line @typescript-eslint/no-require-imports
+//     require('../src/index')
 
-    expect(runMock).toHaveBeenCalled()
-  })
-})
+//     expect(runMock).toHaveBeenCalled()
+//   })
+// })
